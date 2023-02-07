85d65523e365ff3f4996cc0a009d7378e9946c12
IWANDETO <wandettoian@gmail.com>
2023-02-07 20:16:56 +0300

    First variables in Dart

---
+++ dynamic_variables.dart
@@ -0,0 +1,7 @@
+void main() {
+    dynamic number = 10;
+    number = "String";
+    Object name = "Ian Wandeto";
+    name = 10;
+    // We can change the type of variables by declaring Object or dynamic at the beginning.
+}
---
+++ variables.dart
@@ -0,0 +1,13 @@
+void main() {
+    //This files demonstrates the data types in Dart
+    int number = 10;
+    String myname = "Ian Wandeto";
+    double = 9.11;
+
+    // declaring data types using the keyword var
+    var newnumber = 19;
+    var name = "Leroy Sane";
+    var decimalnumber = 17.5;
+    /* Multi Line Comments can 
+    be done this way*/
+}
